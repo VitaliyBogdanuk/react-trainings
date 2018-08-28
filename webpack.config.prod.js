264f91475bd5c565c967c345e1b94d906e544113
VitaliiBohdaniuk <vitalii.bogdaniuk@udelphi.com>
2018-08-28 14:39:54 +0300

    add source map to prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {

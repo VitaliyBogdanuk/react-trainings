eb763cac2344fd99742a41c098d3769f51d676f9
VitaliiBohdaniuk <vitalii.bogdaniuk@udelphi.com>
2018-08-31 14:48:12 +0300

    remove unused files

--- src/global-reducer.jsx
+++ src/global-reducer.jsx
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
-import sumReducer from './sum/reducer'
-
 const appReducer = combineReducers({
-  router: routerReducer,
-  cart: sumReducer
+  router: routerReducer
 })
 
 export default appReducer
